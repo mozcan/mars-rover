d894077dfd4bf5f1a62897e9edca265fdf11c79f
Mustafa ÖZCAN <wustafa.ozcan@gmail.com>
2020-08-23 23:57:28 +0300

    Project Update

--- src/com/mozcan/MoveStrategy.java
+++ src/com/mozcan/MoveStrategy.java
@@ -1,6 +1,5 @@
 package com.mozcan;
 
-import com.mozcan.model.EnumPosition;
 import com.mozcan.service.*;
 
 public class MoveStrategy {
@@ -17,19 +16,5 @@ public class MoveStrategy {
 
     public String moveForwward(int x,int y) {
         return move.moveForward(x,y);
-    }
-
-    public Move getMoveObject(String position) {
-
-        if(EnumPosition.NORTH.getPosition().equalsIgnoreCase(position))
-            return new NorthMove();
-        else if(EnumPosition.SOUTH.getPosition().equalsIgnoreCase(position))
-            return new SouthMove();
-        else if(EnumPosition.WEST.getPosition().equalsIgnoreCase(position))
-            return new WestMove();
-        else if(EnumPosition.EAST.getPosition().equalsIgnoreCase(position))
-            return new EastMove();
-        else
-            return null;
     }
 }
